b811ef8a769037d0a15491fdd62ab3e193811f06
TranHuuAnhVan <bhanhvan1201@gmail.com>
2021-03-03 13:27:00 +0700

    update -htpps

--- src/app/components/register/register/Register.js
+++ src/app/components/register/register/Register.js
@@ -1,3 +1,4 @@
+import { cilDoubleQuoteSansLeft } from '@coreui/icons';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -24,34 +25,31 @@ function Register(props) {
 
     function handleSubmitRegister(event) {
         event.preventDefault();
-        setSubmitted(true);
-       
+        setSubmitted(true);  
         if (username && password && name) {
             setRegister(register);
             alert('submit register thanh cong');
             createUser();
         }
+        console.log('ok');
     }
+    console.log(register);
     return (
         <div className="container register">
             <form id="from-register" className="form" onSubmit={handleSubmitRegister}>
                 <h2>Register With Us</h2>
-                <div className={"form-control" + ((submitted && !username) ? " error" : " ")}>
+                <div className={"form-control" + ((submitted && !username) ? " " : " ")}>
                     <label htmlFor="username">UserName</label>
                     <input name="username" value={username} onChange={handleChangeRegister} type="text" id="username-login" placeholder="Enter username" />
                     <small>Error Username !</small>
                 </div>
-                <div className={"form-control" + ((submitted && !password) ? " error" : " ")}>
+                <div className={"form-control" + ((submitted && !password) ? " " : " ")}>
                     <label htmlFor="password">Password</label>
                     <input name="password" value={password} onChange={handleChangeRegister} type="password" id="password" placeholder="Enter password" />
                     <small>Error message</small>
                 </div>
-                {/* <div className={"form-control" + ((submitted && !passwordConfirm) ? " error" : " ")} style={{display: 'none'}}>
-                    <label htmlFor="passwordConfirm">Confirm Password</label>
-                    <input name="passwordConfirm" value={passwordConfirm} onChange={handleChangeRegister} type="password" id="passwordConfirm" placeholder="Enter password again" />
-                    <small>Error message</small>
-                </div> */}
-                <div className={"form-control" + ((submitted && !name) ? " error" : " ")} >
+                
+                <div className={"form-control" + ((submitted && !name) ? " " : " ")} >
                     <label htmlFor="name">Name</label>
                     <input name="name" value={name} onChange={handleChangeRegister} type="text" id="name" placeholder="Enter Name" />
                     <small>Error Name</small>
